be28358d0b9f1e8c4ab22d292cff5c382805a0e8
leon332157 <leon332157@gmail.com>
2024-10-21 02:40:43 -0400

    fix x86 cmd

--- challenges/rev/armtomic-bomb/build.sh
+++ challenges/rev/armtomic-bomb/build.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 # x86-64
-gcc -Wall -o static/armtomic-bomb-x86-64 -no-pie -fno-stack-protector src.c
+x86_64-linux-gnu-gcc -Wall -o static/armtomic-bomb-x86-64 -no-pie -fno-stack-protector src.c
 # arm v7 linux
 arm-linux-gnueabihf-gcc -Wall -o static/armtomic-bomb-armv7 -no-pie -fno-stack-protector src.c
 # arm v8 linux
