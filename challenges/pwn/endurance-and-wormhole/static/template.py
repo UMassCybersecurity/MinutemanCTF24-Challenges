bb1351648a5739ce63c28bdc1287d49905710327
leon332157 <leon332157@gmail.com>
2024-10-24 19:35:00 -0400

    update template and solve for ret2win

--- challenges/pwn/endurance-and-wormhole/static/template.py
+++ challenges/pwn/endurance-and-wormhole/static/template.py
@@ -1,10 +1,10 @@
 from pwn import *
 
 if args.REMOTE:
-    host = "challs.umasscybersec.org"
-    port = 1337
+    host = "34.75.76.65"
+    port = 9004
     p = remote(host, port)
 else:
-    e = ELF("./static/wormhole")
+    e = ELF("./wormhole")
     context.terminal = ["tmux","splitw","-h"]
     p = process(e.path)
